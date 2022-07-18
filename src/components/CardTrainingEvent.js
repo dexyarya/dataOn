54e093530ac32117ae6052503a2856d4911c67e9
dexyarya <arya.dexy@gmail.com>
2022-07-18 20:02:44 +0700

    T340323 Integration GET API

--- src/components/CardTrainingEvent.js
+++ src/components/CardTrainingEvent.js
@@ -3,22 +3,58 @@ import { Card, Row, Col, Tooltip, Button } from "antd";
 import {
   UserOutlined,
   EnvironmentOutlined,
+  GlobalOutlined,
   StarFilled,
 } from "@ant-design/icons";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import "./ardTrainingEvent.css";
+
+const covertdate = (date) => {
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const dateFormat = new Date(date);
+  const day = dateFormat.getDate();
+  const month = monthNames[dateFormat.getMonth() + 1];
+  const year = dateFormat.getFullYear();
+  const hour = (dateFormat.getHours() < 10 ? "0" : "") + dateFormat.getHours();
+  const minute =
+    (dateFormat.getMinutes() < 10 ? "0" : "") + dateFormat.getMinutes();
+  return `${day} ${month} ${year}, ${hour}:${minute}`;
+};
+
+const endDate = (endDate) => {
+  const dateFormat = new Date(endDate);
+  const hour = (dateFormat.getHours() < 10 ? "0" : "") + dateFormat.getHours();
+  const minute =
+    (dateFormat.getMinutes() < 10 ? "0" : "") + dateFormat.getMinutes();
+  return `${hour}:${minute}`;
+};
 
 function CardTrainingEvent(props) {
   const joinText = "You've joined this class";
+  const isOnline = props.isOnline;
   return (
     <div>
       <Tooltip
         placement="right"
-        title={props.title}
+        title={props.eventName}
         color="#fff"
         overlay={
           <div className="tooltip">
-            <h2 className="titleTooltip">{props.title}</h2>
+            <h2 className="titleTooltip">{props.eventName}</h2>
             <div className="recomen">
               <StarFilled className="iconStar" />
               Recommended Training
@@ -52,24 +88,29 @@ function CardTrainingEvent(props) {
           cardStyle={{
             boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)",
           }}
-          cover={<img alt="image" src={props.img} />}
+          cover={<img alt="image" src={props.image} />}
         >
           <Row justify="between">
-            <Col>
-              <EnvironmentOutlined className="icon" />
+            <Col style={{ marginRight: "3px" }}>
+              {isOnline ? (
+                <GlobalOutlined className="icon" />
+              ) : (
+                <EnvironmentOutlined className="icon" />
+              )}
             </Col>
             <Col className="location">{props.location}</Col>
           </Row>
 
-          <h3 className="title">{props.title}</h3>
+          <h3 className="title">{props.eventName}</h3>
           <p className="description" style={{ marginBottom: "0" }}>
-            {props.description}
+            {/* {props.description} */}
+            {covertdate(props.createdAt)} - {endDate(props.endDate)}
           </p>
           <Row justify="between">
             <Col>
-              <UserOutlined className="icon" />
+              <UserOutlined className="icon" style={{ marginRight: "3px" }} />
             </Col>
-            <Col className="people">{props.people} People register</Col>
+            <Col className="people">{props.participant} People register</Col>
           </Row>
         </Card>
       </Tooltip>
@@ -77,12 +118,12 @@ function CardTrainingEvent(props) {
   );
 }
 
-CardTrainingEvent.propTypes = {
-  location: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  people: PropTypes.number.isRequired,
-};
+// CardTrainingEvent.propTypes = {
+//   location: PropTypes.string.isRequired,
+//   img: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   description: PropTypes.string.isRequired,
+//   people: PropTypes.number.isRequired,
+// };
 
 export default CardTrainingEvent;
